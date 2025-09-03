e567b50bdfa30472fae481687938d033afb03a4e
Mathew Kennedy <matthew_i_kennedy@iCloud.com>
2025-09-02 20:39:56 -0500

    nit: arch config backup

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -70,9 +70,3 @@
 (package! org-caldav)
 (package! org-roam-ui)
 (package! websocket) ;; for org-roam-ui
-
-(package! dirvish)
-(package! harpoon)
-
-(package! elfeed-tube)
-(package! elfeed-tube-mpv)
